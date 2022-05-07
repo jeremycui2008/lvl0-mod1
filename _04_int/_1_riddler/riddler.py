6e3a250c6a4261d311bc6c0ce3a84c5e39240cfe
jeremycui2008 <jeremycuid@gmail.com>
2022-05-06 19:53:02 -0700

    cool

--- _04_int/_1_riddler/riddler.py
+++ _04_int/_1_riddler/riddler.py
@@ -1,4 +1,25 @@
-"""
+
+import math
+from tkinter import simpledialog, messagebox, Tk
+import tkinter as tk
+if __name__ == '__main__':
+    window = Tk()
+    window.withdraw()
+    kyle=0
+    rob = simpledialog.askstring(title='what am I?', prompt='What has cities but no houses forests but no trees and water but no fish?')
+    bob = simpledialog.askstring(title='am what I?', prompt='If eleven plus two equals one, what does nine plus five equal?')
+    bill = simpledialog.askstring(title='I what am?', prompt='You live in a one story house made entirely of redwood. What color would the stairs be?')
+    if bill=='there are no stairs':
+        kyle=kyle+1
+    if bob=='2':
+        kyle=kyle+1
+    if rob=='map':
+        kyle=kyle+1
+    print(kyle)
+
+
+
+"""""
 * Write a python program that asks the user a minimum of 3 riddles.
 
 * You can look at riddles.com if you don't already know any riddles.
--- _04_int/_2_simple_number_adder/simple_adder.py
+++ _04_int/_2_simple_number_adder/simple_adder.py
@@ -1,5 +1,17 @@
+
+
 """
 * Write a Python program that asks the user for two numbers.
 
 * Display the sum of the two numbers to the user
 """
+import math
+from tkinter import simpledialog, messagebox, Tk
+import tkinter as tk
+if __name__ == '__main__':
+    window = Tk()
+    window.withdraw()
+    rob = simpledialog.askinteger(title='first', prompt='select a first number')
+    bob = simpledialog.askinteger(title='second', prompt='select a second number')
+    bill=rob+bob
+    print(bill)
